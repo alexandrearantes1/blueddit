9953b03273445b3286fb7a13aaea1707963229b7
Alexandre Arantes <chan.arantes@gmail.com>
2024-03-06 09:17:56 +0000

    initial django project setup

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgres://jraxrpqe:HDzh46NNYPx08CYl-SJl1P0O4osbff5a@flora.db.elephantsql.com/jraxrpqe")
+os.environ.setdefault("SECRET_KEY", "$^vt*-!y)s5&7$lpzu-8(t_-j$7k9$zy5udl+&b5gk%#19%l@v")
