01b39901705ca6d4893af3859b6931a5b4db5786
Alexandre Arantes <chan.arantes@gmail.com>
2024-03-06 11:11:57 +0000

    fix heroku domain on settings.py

--- blueddit/settings.py
+++ blueddit/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-m5qgn=t!rj+a)t(4_kr(2lwny05g=b32j6qi@54rc=*69bnr&a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
